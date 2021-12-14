6ab766f41ab93fdb770a0821de7290bf6b912de2
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2021-12-13 20:22:45 -0800

    Initial Commit

---
+++ fortuneCookie/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  fortuneCookie
+//
+//  Created by Abel Christy Anand on 12/13/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
